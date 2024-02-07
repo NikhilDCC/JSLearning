b23539b8605d2c08f0d2d04912c451896ef6320b
NikhilDCC <144116752+NikhilDCC@users.noreply.github.com>
2024-02-07 10:00:56 +0530

    Update stringprograms.js
    Added array of strings input program for longest common substring

--- stringprograms.js
+++ stringprograms.js
@@ -59,3 +59,32 @@ longString = "abcabcbb";
 console.log("Longest substring without repeating characters:", longestSubstringWithoutRepeatingChars(longString));
 
 
+//longestCommonSubstring in array of strings as input 
+
+function longestCommonSubstring(strs) {
+    if (!strs.length) return ''; // If the array is empty, return an empty string
+    
+    const shortestString = strs.reduce((shortest, current) => current.length < shortest.length ? current : shortest);
+    let longestSubstring = '';
+
+    for (let i = 0; i < shortestString.length; i++) {
+        for (let j = i + 1; j <= shortestString.length; j++) {
+            const substring = shortestString.substring(i, j);
+            if (strs.every(str => str.includes(substring))) { // Check if the substring exists in all strings
+                if (substring.length > longestSubstring.length) {
+                    longestSubstring = substring;
+                }
+            }
+        }
+    }
+
+    return longestSubstring;
+}
+
+    let arr = ["Amazon", "AmazonHousing.com","AmazonMicrosoft","azonMorgan Stanley"];
+    console.log(longestCommonSubstring(arr));
+//or direct array passed as arg
+    console.log(longestCommonSubstring(['google','amazon','jester']));
+
+
+
